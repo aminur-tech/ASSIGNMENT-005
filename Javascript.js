e72d09c53d87e97bb270ce14343bf695dcddf264
aminurrahman4078 <aminurrahman9793@gmail.com>
2025-08-27 17:36:37 +0600

    Added heart and copy section

--- Javascript.js
+++ Javascript.js
@@ -1,18 +1,20 @@
 
 
-// heart count section
-const heartButtons = document.querySelectorAll(".fa-heart"); 
 
-const heartCount = document.getElementById("heartCount");
 
-let count = 0;
 
-heartButtons.forEach(btn => {
-  btn.addEventListener("click", function() {
-    count++; 
-    heartCount.textContent = count; 
+
+function setupCounter(buttonSelector, displayId) {
+  const buttons = document.querySelectorAll(buttonSelector);
+  const display = document.getElementById(displayId);
+  let count = 0;
+  buttons.forEach(btn => {
+    btn.addEventListener("click", function() {
+      count++;
+      display.textContent = count;
+    });
   });
-});
+}
 
-
-// 
+setupCounter(".fa-heart", "heartCount");
+setupCounter(".copy-btn", "copyCount");
